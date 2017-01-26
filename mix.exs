3709bd23048ddeca86d5a9984fb155afa6eeae1c
Alexei Sholik <alcosholik@gmail.com>
2017-01-26 18:15:27 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -14,25 +14,12 @@ defmodule InetCidr.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.7", only: :dev}]
+    [{:ex_doc, "~> 0.14", only: :dev}]
   end
 
   defp description do
