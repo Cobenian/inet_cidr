c0da19a0ab6d4622595e82da20a31689cafd28c5
Bryan Weber <bryan@cobenian.com>
2024-01-10 08:49:09 -0500

    fixing compiler warnings in test

--- test/inet_cidr_test.exs
+++ test/inet_cidr_test.exs
@@ -5,8 +5,34 @@ defmodule InetCidrTest do
     assert 1 + 1 == 2
   end
 
-  test "parse_cidr! is the same as parse" do
+  test "parse_cidr works with :ok/:error tuples" do
+    assert InetCidr.parse_cidr("192.168.0.0/0", true) ==
+             {:ok, {{0, 0, 0, 0}, {255, 255, 255, 255}, 0}}
+
+    assert InetCidr.parse_cidr("192.168.0.0/2") ==
+             {:error, %RuntimeError{message: "Invalid CIDR: 192.168.0.0/2"}}
+  end
+
+  test "can parse ipv4 cidr block" do
+    assert InetCidr.parse_cidr!("192.168.0.0/0", true) == {{0, 0, 0, 0}, {255, 255, 255, 255}, 0}
+
+    assert InetCidr.parse_cidr!("192.168.0.0/8", true) ==
+             {{192, 0, 0, 0}, {192, 255, 255, 255}, 8}
+
+    assert InetCidr.parse_cidr!("192.168.0.0/15", true) ==
+             {{192, 168, 0, 0}, {192, 169, 255, 255}, 15}
+
     assert InetCidr.parse_cidr!("192.168.0.0/16") == {{192, 168, 0, 0}, {192, 168, 255, 255}, 16}
+    assert InetCidr.parse_cidr!("192.168.0.0/17") == {{192, 168, 0, 0}, {192, 168, 127, 255}, 17}
+    assert InetCidr.parse_cidr!("192.168.0.0/18") == {{192, 168, 0, 0}, {192, 168, 63, 255}, 18}
+    assert InetCidr.parse_cidr!("192.168.0.0/19") == {{192, 168, 0, 0}, {192, 168, 31, 255}, 19}
+    assert InetCidr.parse_cidr!("192.168.0.0/20") == {{192, 168, 0, 0}, {192, 168, 15, 255}, 20}
+    assert InetCidr.parse_cidr!("192.168.0.0/21") == {{192, 168, 0, 0}, {192, 168, 7, 255}, 21}
+    assert InetCidr.parse_cidr!("192.168.0.0/22") == {{192, 168, 0, 0}, {192, 168, 3, 255}, 22}
+    assert InetCidr.parse_cidr!("192.168.0.0/23") == {{192, 168, 0, 0}, {192, 168, 1, 255}, 23}
+    assert InetCidr.parse_cidr!("192.168.0.0/24") == {{192, 168, 0, 0}, {192, 168, 0, 255}, 24}
+    assert InetCidr.parse_cidr!("192.168.0.0/31") == {{192, 168, 0, 0}, {192, 168, 0, 1}, 31}
+    assert InetCidr.parse_cidr!("192.168.0.0/32") == {{192, 168, 0, 0}, {192, 168, 0, 0}, 32}
 
     assert_raise(
       RuntimeError,
@@ -17,60 +43,35 @@ defmodule InetCidrTest do
     )
   end
 
-  test "parse_cidr works with :ok/:error tuples" do
-    assert InetCidr.parse_cidr("192.168.0.0/0", true) ==
-             {:ok, {{0, 0, 0, 0}, {255, 255, 255, 255}, 0}}
-
-    assert InetCidr.parse_cidr("192.168.0.0/2") ==
-             {:error, %RuntimeError{message: "Invalid CIDR: 192.168.0.0/2"}}
-  end
-
-  test "can parse ipv4 cidr block" do
-    assert InetCidr.parse("192.168.0.0/0", true) == {{0, 0, 0, 0}, {255, 255, 255, 255}, 0}
-    assert InetCidr.parse("192.168.0.0/8", true) == {{192, 0, 0, 0}, {192, 255, 255, 255}, 8}
-    assert InetCidr.parse("192.168.0.0/15", true) == {{192, 168, 0, 0}, {192, 169, 255, 255}, 15}
-    assert InetCidr.parse("192.168.0.0/16") == {{192, 168, 0, 0}, {192, 168, 255, 255}, 16}
-    assert InetCidr.parse("192.168.0.0/17") == {{192, 168, 0, 0}, {192, 168, 127, 255}, 17}
-    assert InetCidr.parse("192.168.0.0/18") == {{192, 168, 0, 0}, {192, 168, 63, 255}, 18}
-    assert InetCidr.parse("192.168.0.0/19") == {{192, 168, 0, 0}, {192, 168, 31, 255}, 19}
-    assert InetCidr.parse("192.168.0.0/20") == {{192, 168, 0, 0}, {192, 168, 15, 255}, 20}
-    assert InetCidr.parse("192.168.0.0/21") == {{192, 168, 0, 0}, {192, 168, 7, 255}, 21}
-    assert InetCidr.parse("192.168.0.0/22") == {{192, 168, 0, 0}, {192, 168, 3, 255}, 22}
-    assert InetCidr.parse("192.168.0.0/23") == {{192, 168, 0, 0}, {192, 168, 1, 255}, 23}
-    assert InetCidr.parse("192.168.0.0/24") == {{192, 168, 0, 0}, {192, 168, 0, 255}, 24}
-    assert InetCidr.parse("192.168.0.0/31") == {{192, 168, 0, 0}, {192, 168, 0, 1}, 31}
-    assert InetCidr.parse("192.168.0.0/32") == {{192, 168, 0, 0}, {192, 168, 0, 0}, 32}
-  end
-
   test "can parse ipv6 cidr block" do
-    assert InetCidr.parse("2001:abcd::/0", true) ==
+    assert InetCidr.parse_cidr!("2001:abcd::/0", true) ==
              {{0, 0, 0, 0, 0, 0, 0, 0}, {65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535},
               0}
 
-    assert InetCidr.parse("2001:abcd::/32") ==
+    assert InetCidr.parse_cidr!("2001:abcd::/32") ==
              {{8193, 43981, 0, 0, 0, 0, 0, 0},
               {8193, 43981, 65535, 65535, 65535, 65535, 65535, 65535}, 32}
 
-    assert InetCidr.parse("2001:abcd::/33") ==
+    assert InetCidr.parse_cidr!("2001:abcd::/33") ==
              {{8193, 43981, 0, 0, 0, 0, 0, 0},
               {8193, 43981, 32767, 65535, 65535, 65535, 65535, 65535}, 33}
 
-    assert InetCidr.parse("2001:abcd::/34") ==
+    assert InetCidr.parse_cidr!("2001:abcd::/34") ==
              {{8193, 43981, 0, 0, 0, 0, 0, 0},
               {8193, 43981, 16383, 65535, 65535, 65535, 65535, 65535}, 34}
 
-    assert InetCidr.parse("2001:abcd::/35") ==
+    assert InetCidr.parse_cidr!("2001:abcd::/35") ==
              {{8193, 43981, 0, 0, 0, 0, 0, 0},
               {8193, 43981, 8191, 65535, 65535, 65535, 65535, 65535}, 35}
 
-    assert InetCidr.parse("2001:abcd::/36") ==
+    assert InetCidr.parse_cidr!("2001:abcd::/36") ==
              {{8193, 43981, 0, 0, 0, 0, 0, 0},
               {8193, 43981, 4095, 65535, 65535, 65535, 65535, 65535}, 36}
 
-    assert InetCidr.parse("2001:abcd::/128") ==
+    assert InetCidr.parse_cidr!("2001:abcd::/128") ==
              {{8193, 43981, 0, 0, 0, 0, 0, 0}, {8193, 43981, 0, 0, 0, 0, 0, 0}, 128}
 
-    assert InetCidr.parse("2001:db8::/48") ==
+    assert InetCidr.parse_cidr!("2001:db8::/48") ==
              {{8193, 3512, 0, 0, 0, 0, 0, 0}, {8193, 3512, 0, 65535, 65535, 65535, 65535, 65535},
               48}
   end
@@ -106,6 +107,14 @@ defmodule InetCidrTest do
   test "can parse ipv6 address" do
     assert InetCidr.parse_address!("2001:abcd::") == {8193, 43981, 0, 0, 0, 0, 0, 0}
     assert InetCidr.parse_address!("1:abcd::4") == {1, 43981, 0, 0, 0, 0, 0, 4}
+
+    assert_raise(
+      RuntimeError,
+      "Invalid address: 1:vwxyz::4",
+      fn ->
+        InetCidr.parse_address!("1:vwxyz::4")
+      end
+    )
   end
 
   test "correct address count for ipv4 cidr block" do
